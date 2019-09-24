33048b1139583365d9d979436110b683972d6d55
Eugene Farafonov <eu.farafonov@gmail.com>
2019-09-24 13:18:44 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArr = first.split("").reverse();
+  let secondArr = second.split("").reverse();
+  let resArr = [];
+  
+  for (let i = 0, firstLen = firstArr.length; i < firstLen; i++) {
+    for (let j = 0, secondLen = secondArr.length; j < secondLen; j++) {
+      let ind = i + j;
+      let numb = Number(firstArr[i]) * Number(secondArr[j]);
+
+      writeDataToArray(ind, numb);
+    }
+  }
+
+  function writeDataToArray(ind, numb) {
+    if (resArr[ind] != undefined) {
+      numb += resArr[ind];
+    }
+
+    if (numb > 9) {
+      let balance = numb % 10;
+      let base = (numb - balance) / 10;
+      resArr[ind] = balance;
+      writeDataToArray(ind + 1, base);
+    } else {
+      resArr[ind] = numb;
+    }
+  }
+
+  return resArr.reverse().join("");
 }
